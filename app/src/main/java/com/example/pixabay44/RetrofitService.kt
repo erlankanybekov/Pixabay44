f2b797c4f11b1ab9361412bb977cdf411961a20f
erlankanybekov <erkanybekov@gmail.com>
2022-07-30 19:48:59 +0600

    added

--- app/src/main/java/com/example/pixabay44/RetrofitService.kt
+++ app/src/main/java/com/example/pixabay44/RetrofitService.kt
@@ -5,7 +5,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitService {
-   // https://pixabay.com/api/?key=28914088-0fb59487549a00cfcac299658&q=yellow+flowers&image_type=photo
 
   private  val retrofit = Retrofit.Builder().baseUrl("https://pixabay.com/")
         .addConverterFactory(GsonConverterFactory.create()).build()
